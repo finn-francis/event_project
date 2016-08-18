8c02b020a71e48af45e9840bb085f77484398863
Finn Francis <finn_francis@yahoo.com>
2016-08-18 10:54:42 +0100

    Users can edit their profile description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817183519) do
+ActiveRecord::Schema.define(version: 20160818095233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,11 +47,12 @@ ActiveRecord::Schema.define(version: 20160817183519) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.string   "first_name"
     t.string   "last_name"
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -5,14 +5,17 @@ end
 When(/^they enter their profile details$/) do
   @first_name = Faker::Name.first_name
   @last_name  = Faker::Name.last_name
+  @description = Faker::Lorem.paragraph
 
   fill_in 'profile_first_name', with: @first_name
   fill_in 'profile_last_name', with: @last_name
+  fill_in 'profile_description', with: @description
 end
 
 Then(/^the profile edits will be saved$/) do
   @user = User.first
   expect(@user.profile.first_name).to eq(@first_name)
   expect(@user.profile.last_name).to eq(@last_name)
+  expect(@user.profile.description).to eq(@desciption)
 end
 
